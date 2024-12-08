43be15b2c0219b79397afe8207ae66795432b658
Krishna Shrivastava <krshnshrvstv.gwl@gmail.com>
2024-12-08 19:59:41 +0530

    corsurl

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://krix.netlify.app", // Frontend URL
     methods: ["GET", "POST"],
   },
 });
